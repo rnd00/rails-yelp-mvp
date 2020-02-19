edf4fa07cbdfe6d97f9320de2d5d12f8da0619c4
Ryanda Diedan <rndmx00@gmail.com>
2020-02-19 15:56:06 +0900

    controllers / views / scss

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,46 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    # get single entry
+  end
+
+  def edit
+    # edit single entry
+  end
+
+  def update
+    # what edit actually entails
+  end
+
+  def destroy
+    # and here goes boom
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+    @reviews = Review.where(restaurant: @restaurant)
+  end
 end
